f3090a8e7a734f5838657116ad886f3d0c3e69ad
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-12 13:40:11 -0500

    feat swagger and openapi

---
+++ src/main/java/com/demo/pjglmicrousers/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.demo.pjglmicrousers.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Pjgl micro-users API Users")
+                        .description("API Microservicios de usuarios")
+                        .version("1.0"));
+    }
+}
