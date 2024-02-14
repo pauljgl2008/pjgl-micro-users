5fe285a9b9027a1ae351d09db2a7748d69db0fa2
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-13 21:19:13 -0500

    feat rabbitmq with Spring Cloud Bus

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 pjgl-micro-users spring rest, security, actuator, data, config client
 
 https://docs.spring.io/spring-security/reference/servlet/architecture.html
+
+Docker:
+Correr la imagen de RabbitMQ antes de iniciar el micro:
+- $ docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
+
--- src/main/java/com/demo/pjglmicrousers/config/SecurityConfig.java
+++ src/main/java/com/demo/pjglmicrousers/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.demo.pjglmicrousers.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,12 +13,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(x->x.disable())
+                .csrf(x -> x.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
-                .httpBasic(x->x.disable())
-                .formLogin(x->x.disable());
+                .httpBasic(x -> x.disable())
+                .formLogin(x -> x.disable());
         return http.build();
     }
 
