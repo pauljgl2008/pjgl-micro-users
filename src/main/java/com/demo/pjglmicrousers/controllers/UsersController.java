630f35dd214f7212d5acb9cfc94d1ef2af9c3ee7
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-20 11:32:44 -0500

    fix Timed para Prometheus

--- src/main/java/com/demo/pjglmicrousers/controllers/UsersController.java
+++ src/main/java/com/demo/pjglmicrousers/controllers/UsersController.java
@@ -2,9 +2,9 @@ package com.demo.pjglmicrousers.controllers;
 
 import com.demo.pjglmicrousers.config.UsersProperties;
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,26 +20,42 @@ public class UsersController {
 
     private UsersProperties usersProperties;
 
-    private MeterRegistry registry;
+    private MeterRegistry meterRegistry;
 
-    public UsersController(UsersProperties usersProperties, MeterRegistry registry) {
+    public UsersController(UsersProperties usersProperties, MeterRegistry meterRegistry) {
         this.usersProperties = usersProperties;
-        this.registry = registry;
+        this.meterRegistry = meterRegistry;
+    }
+
+    private final Random random = new Random();
+
+    @GetMapping("/read")
+    @Timed(value = "read.test",histogram = true, percentiles = {0.5, 0.75, 0.95, 0.99, 0.9999})
+    public String read() throws InterruptedException {
+        Counter counter = Counter.builder("api_read_get")
+                .description("a number of requests to /read endpoint")
+                .register(meterRegistry);
+
+        // Imitating call latency
+        Thread.sleep(10 + random.nextLong(50));
+        counter.increment();
+        return "Done";
     }
 
     @GetMapping
-    @Timed("pauljgl.timer")
+    @Timed("pauljgl.getMessage.timed")
     public String getMessage() {
         Timer timer =
-                this.registry.timer("pauljgl.getMessage.time");
-        timer.record( ()->{
+                this.meterRegistry.timer("pauljgl.getMessage.time");
+        timer.record(() -> {
             try {
                 Thread.sleep(new
                         Random().nextInt(1500));
-            } catch (InterruptedException e) {}
+            } catch (InterruptedException e) {
+            }
             System.out.println("Waiting...");
         });
-        log.info("Total time {} ms",timer.totalTime(TimeUnit.MILLISECONDS));
+        log.info("Total time {} ms", timer.totalTime(TimeUnit.MILLISECONDS));
 
 
         return "message: " + this.usersProperties.getMessage();
